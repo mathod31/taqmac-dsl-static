55bd68c47bbb7c0f0604e74df73ca8f0498d281c
Mathieu Olives-Delmas <mathod@hotmail.fr>
2020-02-04 14:49:28 +0100

    init

---
+++ src/main/java/fr/taqmac/Main.java
@@ -0,0 +1,15 @@
+package fr.taqmac;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication(scanBasePackages = { "fr.taqmac" })
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("run");
+        SpringApplication.run(Main.class, args);
+
+    }
+
+}
---
+++ src/main/java/fr/taqmac/Test.java
@@ -0,0 +1,4 @@
+package fr.taqmac;
+
+public class Test {
+}
